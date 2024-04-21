cc18e214eb4179987913974fb4b65d165510772e
Legion12021 <146947925+Legion12021@users.noreply.github.com>
2024-04-21 18:37:09 +0300

    123

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { fetchImages } from './js/pixaday.js';
