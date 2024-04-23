4e7fcc5c1222aa59c71aad5d25c48290612b580b
Legion12021 <146947925+Legion12021@users.noreply.github.com>
2024-04-23 09:16:28 +0300

    123

--- src/js/pixaday.js
+++ src/js/pixaday.js
@@ -1,22 +1,25 @@
 import axios from 'axios';
 
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '42600049-cf2a2f9bce39b2068dfff6d8c';
+
 export async function getImages(query, currentPage = 1) {
-  const URL = 'https://pixabay.com/api/';
-  const API_KEY = '42600049-cf2a2f9bce39b2068dfff6d8c';
+  try {
+    const response = await axios.get(URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 15,
+        page: currentPage,
+      }
+    });
 
-  const queryUser = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-    page: currentPage,
-  });
-
-  const LINK = `${URL}?${queryUser}`;
-
-  const response = await axios.get(LINK);
-
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,41 +1,24 @@
 export function renderImage(images) {
-  return images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<li class ='gallery-item'>
+  return images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `
+      <li class='gallery-item'>
         <a class="gallery-link" href="${largeImageURL}">
-            <img class="gallery-image"
-                src="${webformatURL}"
-                alt="${tags}"
-                width="360"/>
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360"/>
         </a>
         <div class='image-info'>
-            <div class="info">
-                <h3 class = "info-likes">Likes</h3>
-                <p>${likes}</p>
-            </div>
-            <div class="info">
-                <h3 class = "info-views">Views</h3>
-                <p>${views}</p>
-            </div>
-            <div class="info">
-                <h3 class = "info-comments">Comments</h3>
-                <p>${comments}</p>
-            </div>
-            <div class="info">
-                <h3 class = "info-downloads">Downloads</h3>
-                <p>${downloads}</p>
-            </div>
+          ${renderImageInfo(likes, 'Likes')}
+          ${renderImageInfo(views, 'Views')}
+          ${renderImageInfo(comments, 'Comments')}
+          ${renderImageInfo(downloads, 'Downloads')}
         </div>
-    </li>`
-    )
-    .join('');
+      </li>`;
+  }).join('');
+}
+
+function renderImageInfo(value, label) {
+  return `
+    <div class="info">
+      <h3 class="info-label">${label}</h3>
+      <p>${value}</p>
+    </div>`;
 }
